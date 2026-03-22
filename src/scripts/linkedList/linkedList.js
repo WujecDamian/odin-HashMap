08daffe74875f4d48897d546a1bc6e98a0292c90
WujecDamian <wujecdamian12@gmail.com>
2026-03-22 18:20:51 +0100

    feat: create linkedlist function

--- src/scripts/linkedList/linkedList.js
+++ src/scripts/linkedList/linkedList.js
@@ -1,130 +1,123 @@
 import { node } from "./node.js";
 
-export function linkedList(){
-    return{
-        listHead:null,
-        tail:null,
-        size:null,
-        append(value){
-            let newnode=node(value)
-            if(this.listHead===null){
-                this.listHead=newnode
-                return
-            }
+export function linkedList() {
+  return {
+    listHead: null,
+    tail: null,
+    size: null,
+    append(value) {
+      let newnode = node(value);
+      if (this.listHead === null) {
+        this.listHead = newnode;
+        return;
+      }
 
-            let current=this.listHead
-            while(current.nextNode!==null){
-                current=current.nextNode
-            }
-            current.nextNode=newnode
-        },
-        prepend(value){
+      let current = this.listHead;
+      while (current.nextNode !== null) {
+        current = current.nextNode;
+      }
+      current.nextNode = newnode;
+    },
+    prepend(value) {},
+    size() {
+      let size = 1;
+      if (this.listHead === null) {
+        size = 0;
+        return size;
+      }
+      let current = this.listHead;
 
-        },
-        size(){
-            let size=1
-            if(this.listHead===null){
-                size=0
-                return size
-            }
-            let current=this.listHead
-
-             while(current.nextNode!==null){
-                current=current.nextNode
-                size++
-            }
-            return size
-        },
-        head(){
-            if(this.listHead===null){   
-                return undefined
-            }
-            let current=this.listHead
-            return current
-        },
-        tail(){
-            if(this.listHead===null){   
-                return undefined
-            }
-            let current=this.listHead
-            while(current.nextNode!==null){
-                current=current.nextNode
-            }
-            return current
-        },
-        at(index){
-            if(this.listHead===null){   
-                return undefined
-            }
-            let current=this.listHead
-            let i
-            for(i=0;i<index;i++){
-                if(current.nextNode!==null){
-                    current=current.nextNode
-                }
-                else return
-            }
-            return current.value
-        },
-        pop(){
-            if(this.listHead===null){   
-                return undefined
-            }
-            this.listHead=this.listHead.nextNode
-        },
-        contains(value){
-            if(this.listHead===null){   
-                return false
-            }
-            let current=this.listHead
-            while(current.nextNode!==null){
-                if(current.value===value){
-                    return true
-                }
-                current=current.nextNode
-                if(current.value===value){
-                    return true
-                }
-            }
-            return false
-        },
-        findIndex(value){
-            if(this.listHead===null){   
-                return -1
-            }
-            let current=this.listHead
-            let i
-            for(i=0;i<this.size();i++){
-                if(current.nextNode!==null){
-                    if(current.value===value){
-                        return i
-                    }
-                    else if(current.nextNode.value===value){
-                        return i+1
-                    }
-                    current=current.nextNode
-
-                }
-                else{
-                    return -1
-                }
-            }
-            return current.value
-        },
-        toString(){
-            let result=''
-            if(this.listHead===null){
-            return result
-            }
-            let current=this.listHead
-            while(current.nextNode!==null){
-                result+=`( ${current.value} ) -> `
-                current=current.nextNode
-            }
-            result+=`( ${current.value} ) -> `
-
-            return result+"null"
+      while (current.nextNode !== null) {
+        current = current.nextNode;
+        size++;
+      }
+      return size;
+    },
+    head() {
+      if (this.listHead === null) {
+        return undefined;
+      }
+      let current = this.listHead;
+      return current;
+    },
+    tail() {
+      if (this.listHead === null) {
+        return undefined;
+      }
+      let current = this.listHead;
+      while (current.nextNode !== null) {
+        current = current.nextNode;
+      }
+      return current;
+    },
+    at(index) {
+      if (this.listHead === null) {
+        return undefined;
+      }
+      let current = this.listHead;
+      let i;
+      for (i = 0; i < index; i++) {
+        if (current.nextNode !== null) {
+          current = current.nextNode;
+        } else return;
+      }
+      return current.value;
+    },
+    pop() {
+      if (this.listHead === null) {
+        return undefined;
+      }
+      this.listHead = this.listHead.nextNode;
+    },
+    contains(value) {
+      if (this.listHead === null) {
+        return false;
+      }
+      let current = this.listHead;
+      while (current.nextNode !== null) {
+        if (current.value === value) {
+          return true;
         }
+        current = current.nextNode;
+        if (current.value === value) {
+          return true;
+        }
+      }
+      return false;
+    },
+    findIndex(value) {
+      if (this.listHead === null) {
+        return -1;
+      }
+      let current = this.listHead;
+      let i;
+      for (i = 0; i < this.size(); i++) {
+        if (current.nextNode !== null) {
+          if (current.value === value) {
+            return i;
+          } else if (current.nextNode.value === value) {
+            return i + 1;
+          }
+          current = current.nextNode;
+        } else {
+          return -1;
+        }
+      }
+      return current.value;
+    },
+    toString() {
+      let result = "";
+      if (this.listHead === null) {
+        return result;
+      }
+      let current = this.listHead;
+      while (current.nextNode !== null) {
+        result += `( ${current.value} ) -> `;
+        current = current.nextNode;
+      }
+      result += `( ${current.value} ) -> `;
 
-    }
+      return result + "null";
+    },
+  };
 }
